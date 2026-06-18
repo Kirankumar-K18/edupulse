4e3dbfcd65efe21d68c4aed2a2e8474e2bfcce85
Kirankumar-K18 <kk7770438@gmail.com>
2026-06-18 12:25:59 +0530

    Added professional SMTP password reset system

--- backend/smart_lecturer/settings.py
+++ backend/smart_lecturer/settings.py
@@ -158,3 +158,13 @@ LOGGING = {
 CACHES = {
     'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}
 }
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+
+EMAIL_HOST_USER = 'smartlecturerreview18@gmail.com'
+EMAIL_HOST_PASSWORD = 'krql dmzt yatz mdco'
+
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
